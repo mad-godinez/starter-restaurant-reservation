19321ad99735604dc310ed9f73970e05ea5dda27
Mad Godinez <madgodinez@gmail.com>
2022-05-15 14:04:17 -0500

    adding cors to reservation router

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -4,18 +4,20 @@ require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
 const express = require("express");
 const cors = require("cors");
+const app = express();
 
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 const reservationsRouter = require("./reservations/reservations.router");
 
-const app = express();
-
+/***** enables CORS from frontend *****/ 
 app.use(cors());
 app.use(express.json());
 
+/***** ROUTES *****/
 app.use("/reservations", reservationsRouter);
 
+/***** ERROR HANDLERS *****/
 app.use(notFound);
 app.use(errorHandler);
 
--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -6,6 +6,9 @@
 
 const router = require("express").Router();
 const controller = require("./reservations.controller");
+const cors = require("cors");
+
+router.use(cors());
 
 router.route("/").get(controller.list);
 
