0a655431be0340c47f0fa7f50363e0a043821664
Mad Godinez <madgodinez@gmail.com>
2022-05-15 13:38:57 -0500

    seeded the reservation database with data and connected frontend API to backend db

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -1,5 +1,23 @@
+/***** Data Types Example: 
+ {
+  "first_name": "Rick",
+  "last_name": "Sanchez",
+  "mobile_number": "202-555-0164",
+  "reservation_date": "2020-12-31",
+  "reservation_time": "20:00:00",
+  "people": 6,
+  "created_at": "2020-12-10T08:30:32.326Z",
+  "updated_at": "2020-12-10T08:30:32.326Z"
+},
+*****/
 exports.up = function (knex) {
   return knex.schema.createTable("reservations", (table) => {
+    table.string("first_name");
+    table.string("last_name");
+    table.string("mobile_number");
+    table.date("reservation_date");
+    table.time("reservation_time");
+    table.smallint("people");
     table.increments("reservation_id").primary();
     table.timestamps(true, true);
   });
--- back-end/src/db/seeds/00-reservations.js
+++ back-end/src/db/seeds/00-reservations.js
@@ -1,3 +1,8 @@
-exports.seed = function (knex) {
-  return knex.raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE");
+let reservation_details = require('./00-reservations.json');
+
+exports.seed = function (knex) { 
+  // reservation_details = JSON.stringify(reservation_details);
+  return knex
+          .raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
+          .then(()=> knex("reservations").insert(reservation_details));
 };
