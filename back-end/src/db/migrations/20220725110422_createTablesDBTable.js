8433eb73eaedaa9235b395e222d549f94b10e8c4
Mad Godinez <madgodinez@gmail.com>
2022-07-25 11:41:38 -0500

    tables migrations and seed files successfully connected to sql db

--- back-end/src/db/migrations/20220725110422_createTablesDBTable.js
+++ back-end/src/db/migrations/20220725110422_createTablesDBTable.js
@@ -1,8 +1,25 @@
+/***** Data Types Example: 
+  {
+    "table_name": "Bar #1",
+    "capacity": 1
+  },
+*****/
+// const knex = require("../connection");
+// const reservations = await knex("reservations").select("*");
 
 exports.up = function(knex) {
-  
+  return knex.schema.dropTableIfExists("tables").then(()=>{
+    return knex.schema.createTable("tables", (table)=>{
+      table.string("table_name");
+      table.smallint("capacity");
+      table.increments("table_id").primary();
+      table.smallint("reservation_id");
+      table.foreign("reservation_id").references("reservations.reservation_id");//.inTable("reservations");
+      table.timestamps(true, true);
+    });
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("tables");
 };
